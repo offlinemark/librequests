933b04c974a83c74c37d10f671b6b6f8b65af1d4
Mark Mossberg <mark.mossberg@gmail.com>
2014-03-28 02:25:17 -0400

    update examples

--- examples/post_nodata.c
+++ examples/post_nodata.c
@@ -10,7 +10,7 @@ int main(int argc, const char *argv[])
     REQ req; // declare struct used to store data
     CURL *curl = requests_init(&req, "http://www.posttestserver.com/post.php"); // setup
 
-    requests_post(curl, &req, NULL, 0); // submit POST request
+    requests_post(curl, &req, NULL); // submit POST request
     printf("Request URL: %s\n", req.url);
     printf("Response Code: %lu\n", req.code);
     printf("Response Size: %zu\n", req.size);
